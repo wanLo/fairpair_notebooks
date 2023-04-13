1f349ad38e0fa0cb471e2a8e1cc5b50ed6fc4e83
wanLo <mail@geahnert.de>
2023-04-13 12:09:42 +0200

    Add pairwise comparisons with BTL-model

--- fairpair/fairgraph.py
+++ fairpair/fairgraph.py
@@ -25,14 +25,14 @@ class FairPairGraph(nx.DiGraph):
         self.label_minority(Nm)
 
 
-    def label_minority(self, Nm: int, attribute="minority", random=False, seed: int | None = None):
+    def label_minority(self, Nm: int, attr="minority", random=False, seed: int | None = None):
         '''
         Label a subset of nodes as minority
 
         Parameters
         ----------
         - Nm: number of minority nodes
-        - attribute: name of the node attribute to use as a group label
+        - attr: name of the node attribute to use as a group label
         - random: whether to shuffle nodes before assigning minority attribute
         - seed: seed for the random number generator
         '''
@@ -45,45 +45,79 @@ class FairPairGraph(nx.DiGraph):
             rng.shuffle(labels)
         
         labels_dict = dict(zip(self.nodes, labels))
-        nx.function.set_node_attributes(self, labels_dict, attribute)
+        nx.function.set_node_attributes(self, labels_dict, attr)
 
 
-    def group_assign_scores(self, nodes: list, attribute="score", distr = Distributions.uniform_distr, **kwargs):
+    def group_assign_scores(self, nodes: list, attr="score", distr = Distributions.uniform_distr, **kwargs):
         '''
         Randomly draw scores from a distribution and assign them to the nodes of a subgraph
 
         Parameters
         ----------
         - nodes: list of nodes to assign scores to
-        - attribute: name of the node attribute for storing scores
+        - attr: name of the node attribute for storing scores
         - distr: distribution function with required keyword argument n (number of nodes)
         - **kwargs: keyword arguments passed to distr function
         '''
         scores_dict = dict(zip(nodes, distr(n = len(nodes), **kwargs)))
-        nx.function.set_node_attributes(self, scores_dict, attribute)
+        nx.function.set_node_attributes(self, scores_dict, attr)
+
+
+    def compare_pair(self, i, j, k = 1, node_attr="score", weight_attr="weight", wins_attr="wins", seed: int | None = None):
+        '''
+        Compares nodes i and j using the BTL-formula, k times (binomial distribution).
+
+        Adds a weighted, directed edge between i and j, or updates the edge if the pair had already been compared before.
+
+        Parameters
+        ----------
+        - i: the first node to compare
+        - j: the second node to compare
+        - k: how often to compare the nodes
+        - node_attr: name of the node attribute for storing scores
+        - weight_attr: name of the edge attribute for storing weights
+        - wins_attr: name of the edge attribute for storing #wins
+        - seed: seed for the random number generator
+        '''
+        rng = np.random.default_rng(seed=seed)
+        prob = self.nodes[j][node_attr] / (self.nodes[i][node_attr] + self.nodes[j][node_attr])
+        wins_j = rng.binomial(k, prob)
+
+        # incorporate the results of previous comparisons
+        if (self.has_edge(i, j) and self.has_edge(j, i) and wins_attr in self.edges[(i, j)] and wins_attr in self.edges[(j, i)]):
+            wins_j += self.edges[(i,j)][wins_attr]
+            k += self.edges[(i,j)][wins_attr] + self.edges[(j,i)][wins_attr]
+
+        edge_i_j = {wins_attr: wins_j, # total wins of j over i, arrows are towards the winners
+                    weight_attr: wins_j/k} # compared strength
+        self.add_edge(i, j, **edge_i_j)
+
+        edge_j_i = {wins_attr: k - wins_j, # the comparison in the other way
+                    weight_attr: 1 - wins_j/k}
+        self.add_edge(j, i, **edge_j_i)
 
 
     @cached_property
-    def minority(self, attribute="minority") -> nx.Graph:
+    def minority(self, attr="minority") -> nx.Graph:
         '''Returns a read-only graph view of the minority subgraph, see nx.subgraph_view'''
-        return nx.graphviews.subgraph_view(self, filter_node=lambda x: x in self.minority_nodes(attribute=attribute))
+        return nx.graphviews.subgraph_view(self, filter_node=lambda x: x in self.minority_nodes(attr=attr))
 
 
     @cached_property
-    def minority_nodes(self, attribute="minority") -> list:
+    def minority_nodes(self, attr="minority") -> list:
         '''Returns a list of minority nodes'''
         # TODO: implement this using nx.classes.reportviews.NodeView
-        return [x for x,y in self.nodes(data=attribute) if y]
+        return [x for x,y in self.nodes(data=attr) if y]
 
 
     @cached_property
-    def majority(self, attribute="minority") -> nx.Graph:
+    def majority(self, attr="minority") -> nx.Graph:
         '''Returns a read-only graph view of the majority subgraph, see nx.subgraph_view'''
-        return nx.graphviews.subgraph_view(self, filter_node=lambda x: x in self.majority_nodes(attribute=attribute))
+        return nx.graphviews.subgraph_view(self, filter_node=lambda x: x in self.majority_nodes(attr=attr))
 
 
     @cached_property
-    def majority_nodes(self, attribute="minority") -> list:
+    def majority_nodes(self, attr="minority") -> list:
         '''Returns a list of minority nodes'''
         # TODO: implement this using nx.classes.reportviews.NodeView
-        return [x for x,y in self.nodes(data=attribute) if not y]
+        return [x for x,y in self.nodes(data=attr) if not y]
