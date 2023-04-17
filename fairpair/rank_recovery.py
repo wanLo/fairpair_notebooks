9dd0636487bae5d1c65d1ad7ee7f98cc2eedf6dd
wanLo <mail@geahnert.de>
2023-04-17 16:49:06 +0200

    Add NDCG and MSE implementations

--- fairpair/rank_recovery.py
+++ fairpair/rank_recovery.py
@@ -1,25 +1,33 @@
+from typing import Tuple
+
+import pandas as pd
 import networkx as nx
+from sklearn.metrics import ndcg_score, mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
 
 from .fairgraph import FairPairGraph
 from .recovery_baselines import *
 
 class RankRecovery:
 
-    def __init__(self, G:FairPairGraph, class_attr='minority', weight_attr='weight'):
+    def __init__(self, G:FairPairGraph, class_attr='minority', weight_attr='weight', score_attr='score'):
         '''
         Initialize the ranking
 
         Parameters
         ----------
         - G: the FairPairGraph from which a ranking will be recovered
-        - 
+        - class_attr: name of the node attribute to use as a group label
+        - weight_attr: name of the edge attribute for storing weights
+        - score_attr: name of the node attribute for storing scores
         '''
         self.G = G
         self.class_attr = class_attr
         self.weight_attr = weight_attr
+        self.score_attr = score_attr
 
     
-    def apply(self, rank_using=rankCentrality, **kwargs) -> dict:
+    def apply(self, rank_using=rankCentrality, **kwargs) -> Tuple[dict, list]:
         '''
         Helper for applying a ranking function to a FairPairGraph.
         Preserves node names and only calculates the ranking on the
@@ -56,5 +64,48 @@ class RankRecovery:
         ranking = {node: score for node, score in sorted(ranking.items(), key=lambda item: item[1], reverse=True)}
 
         # print with original score and group membership
+        data = []
         for node, rank_score in ranking.items():
-            print(node, self.G.nodes[node]['score'], rank_score)
+            data.append((node, self.G.nodes[node]['score'], rank_score))
+        return pd.DataFrame(data, columns=['node', 'orig score', 'rank score'])
+
+    def calc_NDCG(self, subgraph:FairPairGraph, ranking:dict, **kwargs) -> float:
+        '''
+        Calculates the normalized discounted cumulative gain (NDCG)
+        for a ranking given initial scores from subgraph
+
+        Parameters
+        ----------
+        - subgraph: a FairPairGraph subgraph 
+        - ranking: dict of nodes and their ranking results
+        - **kwargs: keyword arguments to be passed to sklearn's ndcg_score()
+        '''
+        # extract the ranking scores in the order of subgraph.nodes
+        scores_predicted = [ranking[node] for node in subgraph.nodes if node in ranking]
+        if len(scores_predicted) < 2:
+            raise ValueError('subgraph must have at least 2 nodes contained in ranking to calculate NDCG')
+        # extract true scores for the nodes
+        scores_true = [score for node, score in subgraph.nodes(data=self.score_attr) if node in ranking]
+        return ndcg_score([scores_true], [scores_predicted], **kwargs)
+
+
+    def calc_MSE(self, subgraph:FairPairGraph, ranking:dict, **kwargs) -> float:
+        '''
+        Calculates the mean square error (MSE) for a ranking
+        (scores normalized to (0,1)) given initial scores from subgraph
+
+        Parameters
+        ----------
+        - subgraph: a FairPairGraph subgraph 
+        - ranking: dict of nodes and their ranking results
+        - **kwargs: keyword arguments to be passed to sklearn's mean_squared_error()
+        '''
+
+        # extract the ranking scores in the order of subgraph.nodes
+        scores_predicted = [ranking[node] for node in subgraph.nodes if node in ranking]
+        # normalize to (0,1) to compare to true scores
+        scaler = MinMaxScaler().fit([[score] for score in list(ranking.values())]) # shape: (n_samples, n_features=1)
+        scores_predicted = scaler.transform([[score] for score in scores_predicted])
+        # extract true scores for the nodes
+        scores_true = [score for node, score in subgraph.nodes(data=self.score_attr) if node in ranking]
+        return mean_squared_error(scores_true, scores_predicted.flatten())
