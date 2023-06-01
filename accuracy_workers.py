6a6971a6e52657dbe99cec4dcdb667ca4b4af5ea
wanLo <mail@geahnert.de>
2023-06-01 20:26:04 +0200

    Add wrapper for Fairness-Aware PageRank

---
+++ accuracy_workers.py
@@ -0,0 +1,85 @@
+from scipy.stats import norm
+from scipy import integrate
+import mpmath
+import numpy as np
+
+from fairpair import *
+
+def get_cutoff_accuracy(trial:int, N=400, Nm=200):
+    accuracy = []
+    H = FairPairGraph()
+    H.generate_groups(N, Nm) # same size groups
+    H.group_assign_scores(nodes=H.nodes, loc=0, scale=1) # general score distribution
+    sampler = RandomSampling(H, warn=False)
+    ranker = RankRecovery(H)
+    ranking = None
+    for j in range(101):
+        sampler.apply(iter=100, k=1)
+        for cutoff in [0.3, 0.4, 0.5, 0.6, 0.7, 0.8]:
+            # make sure to use separate directories for each thread
+            ranking, other_nodes = ranker.apply(rank_using='fairPageRank', cutoff=cutoff, path=f'data/tmp{trial}')
+            if len(other_nodes) == 0:
+                tau = weighted_tau(H, ranking)
+                accuracy.append((trial, j*100, cutoff, tau))
+    return accuracy
+
+
+def get_simulated_cutoff(trial:int, method:rankCentrality, N=400, Nm=200):
+    accuracy = []
+    H = FairPairGraph()
+    H.generate_groups(N, Nm) # same size groups
+    H.group_assign_scores(nodes=H.nodes, loc=0, scale=1) # general score distribution
+    sampler = RandomSampling(H, warn=False)
+    ranking = None
+    for j in range(101):
+        sampler.apply(iter=10, k=1)
+        for cutoff in [0.4]:
+            # simulate cut-off
+            G = FairPairGraph()
+            edges = H.edges(data='weight')
+            edges = [(outgoing, incoming, 1) for outgoing, incoming, weight in edges if weight>=cutoff]
+            G.add_weighted_edges_from(edges)
+            ranker = RankRecovery(G)
+            if method == 'fairPageRank':
+                name = method
+                ranking, other_nodes = ranker.apply(rank_using='fairPageRank', cutoff=0, path=f'data/tmp{trial}')
+            else:
+                name = method.__name__
+                ranking, other_nodes = ranker.apply(rank_using=method) # by default, apply rankCentrality method
+            if len(other_nodes) == 0:
+                tau = weighted_tau(H, ranking)
+                accuracy.append((trial, j*10, cutoff, tau, name))
+    return accuracy
+
+
+def get_method_accuracy(trial:int, method:rankCentrality, N=400, Nm=200):
+    accuracy = []
+    H = FairPairGraph()
+    H.generate_groups(N, Nm) # same size groups
+    H.group_assign_scores(nodes=H.nodes, loc=0, scale=0.86142674) # general score distribution
+    #H.group_add_scores(nodes=H.minority_nodes, loc=-1.43574282, scale=0.43071336) # add bias to unpriviledged group
+    #H.group_add_scores(nodes=H.minority_nodes, loc=-10, scale=0.43071336)
+    sampler = RandomSampling(H, warn=False)
+    ranker = RankRecovery(H)
+    ranking = None
+    for j in range(101):
+        sampler.apply(iter=10, k=1)
+        if method == 'fairPageRank':
+            name = method
+            ranking, other_nodes = ranker.apply(rank_using=method, path=f'data/tmp{trial}')
+        else:
+            name = method.__name__
+            ranking, other_nodes = ranker.apply(rank_using=method) # by default, apply rankCentrality method
+        if len(other_nodes) == 0:
+            tau = weighted_tau(H, ranking)
+            accuracy.append((trial, j*10, tau, 'Overall', name))
+            #tau = weighted_tau(H, ranking, H.majority)
+            #accuracy.append((trial, j*10, tau, 'Priviledged', method.__name__))
+            #tau = weighted_tau(H, ranking, H.minority)
+            #accuracy.append((trial, j*10, tau, 'Unpriviledged', method.__name__))
+            #tau = weighted_tau_separate(H, ranking, H.majority)
+            #accuracy.append((trial, j*10, tau[0], 'Priviledged within-group', method.__name__))
+            #accuracy.append((trial, j*10, tau[1], 'Between groups', method.__name__))
+            #tau = weighted_tau_separate(H, ranking, H.minority, calc_between=False)
+            #accuracy.append((trial, j*10, tau[0], 'Unpriviledged within-group', method.__name__))
+    return accuracy
--- fairpair/rank_recovery.py
+++ fairpair/rank_recovery.py
@@ -1,10 +1,60 @@
 from typing import Tuple
+from pathlib import Path
+import os
 
 import pandas as pd
 import networkx as nx
 
 from .fairgraph import FairPairGraph
 from .recovery_baselines import *
+
+
+def fairPageRank(G:FairPairGraph, cutoff=0.4, phi=0.5, path='data/tmp'):
+    '''A wrapper for the C++-based implementation of Fairness-Aware PageRank (Tsioutsiouliklis et al., 2021)'''
+    # get all edges with weights higher than or equal to cutof
+    edges = G.edges(data='weight')
+    edges = [(outgoing, incoming) for outgoing, incoming, weight in edges if weight>=cutoff]
+    
+    # write edgelist
+    graphfile_path = Path(path,'out_graph.txt')
+    with open(graphfile_path, 'w') as file:
+        file.write(f'{len(G.nodes)}\n')
+        for edge in edges:
+            file.write(f'{edge[0]} {edge[1]}\n')
+    
+    # write nodelist with groups
+    nodes = G.nodes(data='minority')
+    nodes = [(node, 1) if minority else (node, 0) for node, minority in nodes]
+    community_path = Path(path,'out_community.txt')
+    with open(community_path, 'w') as file:
+        file.write('2\n') # we always have two groups
+        for node in nodes:
+            file.write(f'{node[0]} {node[1]}\n')
+    
+    # write desired community sizes
+    sizes_path = Path(path,'sizes.txt')
+    with open(sizes_path, 'w') as file:
+        file.write(f'0 {1-phi}\n') # priviledged group
+        file.write(f'1 {phi}\n') # unpriviledged group
+    
+    # run the compiled fairPageRank program
+    # make sure that pagerank.out is located in the path directory
+    dir = os.getcwd()
+    os.chdir(path)
+    os.system('./pagerank.out -c sizes.txt > /dev/null') # run with muted stdout
+    #os.system(f'./residual_optimization.out {phi} > /dev/null') # run with muted stdout
+    os.chdir(dir)
+
+    # read the finished file
+    #result_path = Path(path,'out_pagerank_pagerank.txt')
+    result_path = Path(path,'out_lfpr_p_pagerank.txt')
+    #result_path = Path(path,'out_excess_sensitive_pagerank.txt')
+    with open(result_path, 'r') as file:
+        ranking = file.read().splitlines()
+    ranking = [float(score) for score in ranking]
+
+    return ranking
+
 
 class RankRecovery:
 
@@ -33,6 +83,7 @@ class RankRecovery:
         Parameters
         ----------
         - rank_using: a function that recovers a ranking (list) from an adjacency matrix
+            OR 'fairPageRank', if Fairness-Aware PageRank should be applied
         - **kwargs: keyword arguments to be passed to the ranking function
 
         Returns
@@ -45,14 +96,18 @@ class RankRecovery:
         other_nodes = [node for node in self.G.nodes if node not in connected_nodes]
 
         if len(other_nodes) == 0: # only apply ranking recovery if strongly connected
-            adjacency = nx.linalg.graphmatrix.adjacency_matrix(connected_graph, weight=self.weight_attr)
+            if rank_using == 'fairPageRank':
+                ranking = fairPageRank(self.G, **kwargs)
+                ranking = dict(zip(connected_nodes, ranking))
+            else:
+                adjacency = nx.linalg.graphmatrix.adjacency_matrix(connected_graph, weight=self.weight_attr)
 
-            # The GNNRank implementation generally assumes i->j means "i beats j", while we mean the opposite
-            adjacency = adjacency.transpose()
-            
-            ranking = rank_using(adjacency, **kwargs)
-            ranking = [float(abs(score)) if isinstance(score, complex) else float(score) for score in ranking]
-            ranking = dict(zip(connected_nodes, ranking)) # nodes might have specific names, so we return a dict
+                # The GNNRank implementation generally assumes i->j means "i beats j", while we mean the opposite
+                adjacency = adjacency.transpose()
+                
+                ranking = rank_using(adjacency, **kwargs)
+                ranking = [float(abs(score)) if isinstance(score, complex) else float(score) for score in ranking]
+                ranking = dict(zip(connected_nodes, ranking)) # nodes might have specific names, so we return a dict
         else: ranking = None
 
         return ranking, other_nodes
