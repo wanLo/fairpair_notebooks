f4f62ce89bc0e8fafb3bf8cc8df5434956d40310
wanLo <mail@geahnert.de>
2023-06-22 18:43:44 +0000

    Add GNNRank accuracy experiments

--- accuracy_workers.py
+++ accuracy_workers.py
@@ -2,8 +2,14 @@ from scipy.stats import norm
 from scipy import integrate
 import mpmath
 import numpy as np
+import torch
 
 from fairpair import *
+
+import sys
+sys.path.append('../GNNRank/')
+from src.param_parser import ArgsNamespace # just import the class, not the parser
+from src.Trainer import Trainer
 
 def get_cutoff_accuracy(trial:int, N=400, Nm=200):
     accuracy = []
@@ -53,7 +59,7 @@ def get_simulated_cutoff(trial:int, method:rankCentrality, N=400, Nm=200):
     return accuracy
 
 
-def get_method_accuracy(trial:int, method:rankCentrality, N=400, Nm=200):
+def get_method_accuracy(trial:int, method=rankCentrality, N=400, Nm=200):
     accuracy = []
     H = FairPairGraph()
     H.generate_groups(N, Nm) # same size groups
@@ -62,7 +68,7 @@ def get_method_accuracy(trial:int, method:rankCentrality, N=400, Nm=200):
     sampler = RandomSampling(H, warn=False)
     ranker = RankRecovery(H)
     ranking = None
-    for j in range(101):
+    for j in range(11):
         sampler.apply(iter=100, k=1)
         if method == 'fairPageRank':
             name = method
@@ -82,4 +88,42 @@ def get_method_accuracy(trial:int, method:rankCentrality, N=400, Nm=200):
             #accuracy.append((trial, j*10, tau[1], 'Between groups', name))
             #tau = weighted_tau_separate(H, ranking, H.minority, calc_between=False)
             #accuracy.append((trial, j*10, tau[0], 'Unpriviledged within-group', name))
+    return accuracy
+
+
+def get_GNNRank_accuracy(trial:int):
+    accuracy = []
+    args = ArgsNamespace(AllTrain=True, ERO_style='uniform', F=70, Fiedler_layer_num=5, K=20, N=350, SavePred=False, all_methods=['DIGRAC', 'ib'],
+                     alpha=1.0, baseline='syncRank', cuda=True, data_path='/home/jovyan/GNNRank/src/../data/', dataset='fairPair_test/',
+                     debug=False, device=torch.device(type='cuda'), dropout=0.5, early_stopping=200, epochs=1000, eta=0.1, fill_val=0.5, hidden=8, hop=2,
+                     load_only=False, log_root='/home/jovyan/GNNRank/src/../logs/', lr=0.01, no_cuda=False, num_trials=1, optimizer='Adam', p=0.05,
+                     pretrain_epochs=50, pretrain_with='dist', regenerate_data=True, season=1990, seed=31, seeds=[10], sigma=1.0, tau=0.5, test_ratio=1,
+                     train_ratio=1, train_with='proximal_baseline', trainable_alpha=False, upset_margin=0.01, upset_margin_coeff=0, upset_ratio_coeff=1.0, weight_decay=0.0005)
+    torch.manual_seed(args.seed)
+
+    H = FairPairGraph()
+    H.generate_groups(400, 200) # same size groups
+    H.assign_skills(loc=0, scale=0.86142674) # general skill distribution
+    #H.assign_bias(nodes=H.minority_nodes, loc=-1.43574282, scale=0.43071336) # add bias to unprivileged group
+    sampler = RandomSampling(H, warn=False)
+    ranking = None
+    for j in range(11):
+        sampler.apply(iter=100, k=1)
+        if (nx.is_strongly_connected(H)):
+            adj = nx.linalg.graphmatrix.adjacency_matrix(H, weight='weight') # returns a sparse matrix
+            trainer = Trainer(args, random_seed=10, save_name_base='test', adj=adj) # initialize with the given adjacency matrix
+            save_path_best, save_path_latest = trainer.train(model_name='ib')
+            score, pred_label = trainer.predict_nn(model_name='ib', model_path=save_path_best, A=adj, GNN_variant='proximal_baseline')
+            ranking = {key: 1-score for key, score in enumerate(score.cpu().detach().numpy())}
+            tau = weighted_tau(H, ranking)
+            accuracy.append((trial, j*100, tau, 'Overall'))
+            #tau = weighted_tau(H, ranking, H.majority)
+            #accuracy.append((trial, j*10, tau, 'Priviledged'))
+            #tau = weighted_tau(H, ranking, H.minority)
+            #accuracy.append((trial, j*10, tau, 'Unpriviledged'))
+            #tau = weighted_tau_separate(H, ranking, H.majority)
+            #accuracy.append((trial, j*10, tau[0], 'Priviledged within-group'))
+            #accuracy.append((trial, j*10, tau[1], 'Between groups'))
+            #tau = weighted_tau_separate(H, ranking, H.minority, calc_between=False)
+            #accuracy.append((trial, j*10, tau[0], 'Unpriviledged within-group'))
     return accuracy
