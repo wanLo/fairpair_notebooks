2f194426cec6c3927438bb24060d53872226f104
wanLo <mail@geahnert.de>
2023-10-30 15:21:07 +0100

    Post-processing on a larger scale

---
+++ post_processing_evaluation.py
@@ -0,0 +1,19 @@
+from itertools import product
+import multiprocessing
+
+import pandas as pd
+
+from post_processing_workers import *
+
+
+if __name__ == '__main__':
+
+    tasks = list(product(range(10), ['RandomSampling', 'OversampleMinority', 'RankSampling'],
+                         ['davidScore', 'randomRankRecovery', 'rankCentrality', 'GNNRank'], [False, True])) # trial, sampling_method, ranking_method, apply_bias
+
+    pool = multiprocessing.Pool()
+    accuracy = pool.starmap(post_process, tasks)
+
+    accuracy = [result for pool in accuracy for result in pool]
+    accuracy = pd.DataFrame(accuracy, columns=['trial', 'iteration', 'value', 'bias_applied', 'sampling strategy', 'recovery method', 'metric', 'group'])
+    accuracy.to_csv('./data/post_processing/FAstarIR_10trials.csv', index=False)
