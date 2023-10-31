def4958b5518e6482da2c35e220504ad70ee8ce6
wanLo <mail@geahnert.de>
2023-10-31 17:20:45 +0100

    Add Post-Processing for Exposure with EPIRA

--- post_processing_evaluation.py
+++ post_processing_evaluation.py
@@ -11,9 +11,11 @@ if __name__ == '__main__':
     tasks = list(product(range(10), ['RandomSampling', 'OversampleMinority', 'RankSampling'],
                          ['davidScore', 'randomRankRecovery', 'rankCentrality', 'GNNRank'], [False, True])) # trial, sampling_method, ranking_method, apply_bias
 
+    #tasks = list(product(range(10), ['RandomSampling'], ['davidScore'], [True]))
+
     pool = multiprocessing.Pool()
-    accuracy = pool.starmap(post_process, tasks)
+    accuracy = pool.starmap(post_process_epira, tasks)
 
     accuracy = [result for pool in accuracy for result in pool]
     accuracy = pd.DataFrame(accuracy, columns=['trial', 'iteration', 'value', 'bias_applied', 'sampling strategy', 'recovery method', 'metric', 'group'])
-    accuracy.to_csv('./data/post_processing/FAstarIR_10trials.csv', index=False)
+    accuracy.to_csv('./data/post_processing/EPIRA_10trials.csv', index=False)
