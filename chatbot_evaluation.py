de4e69fca7437d7f0ba994d8f874d10d5d2562f2
wanLo <mail@geahnert.de>
2024-09-26 11:53:58 +0200

    Add experiments with Chatbot Arena

---
+++ chatbot_evaluation.py
@@ -0,0 +1,92 @@
+import pandas as pd
+
+from itertools import product
+import multiprocessing
+
+from fairpair import *
+
+def ranking_evaluation(trial:int, sampling_method:str, ranking_method:str, group_attribute:str, benchmark:str):
+
+    #file = '../fairpair/data/chatbot_arena_results/basicMethods_correlations_full_dataset.csv'
+    file = '../fairpair/data/chatbot_arena_results/GNNRank_correlations_full_dataset.csv'
+
+    data = pd.read_csv(file)
+
+    filtered_df = data[data['ranker'] == ranking_method]#.copy()
+    #del data
+    
+    filtered_df = filtered_df[filtered_df['trial'] == trial] 
+    filtered_df = filtered_df[filtered_df['sampling method'] == sampling_method]
+    filtered_df = filtered_df[filtered_df['group attribute'] == group_attribute] 
+    filtered_df = filtered_df[filtered_df['benchmark'] == benchmark] 
+    filtered_df = filtered_df.reset_index(drop=True)
+
+    first_iteration = filtered_df.iteration.unique()[0]
+
+    base_scores = filtered_df[(filtered_df.iteration == first_iteration)]['skill score'].to_dict()
+    all_nodes = list(filtered_df[filtered_df.iteration == first_iteration].index)
+    majority_nodes = list(filtered_df[(filtered_df.iteration == first_iteration) & (filtered_df.group == 'Privileged')].index)
+    minority_nodes = list(filtered_df[(filtered_df.iteration == first_iteration) & (filtered_df.group == 'Unprivileged')].index)
+
+    results = pd.DataFrame()
+
+    for iteration in filtered_df.iteration.unique():
+
+        _tmp_df = filtered_df[filtered_df.iteration == iteration].reset_index(drop=True)
+        ranking = _tmp_df['rank'].to_dict()
+
+        iter_results = pd.DataFrame(columns=['trial', 'iteration', 'value', 'sampling strategy', 'recovery method',
+                                             'group attribute', 'benchmark', 'metric', 'group'])
+
+        if len(filtered_df):
+            tau = weighted_tau_nodes(base_scores, ranking, subgraph_nodes=all_nodes, complementary_nodes=[])
+            iter_results = pd.concat([iter_results, pd.DataFrame([{'value': tau, 'metric': 'tau', 'group': 'Overall'}])])
+
+            tau = weighted_tau_nodes(base_scores, ranking, subgraph_nodes=majority_nodes, complementary_nodes=minority_nodes)
+            iter_results = pd.concat([iter_results, pd.DataFrame([{'value': tau, 'metric': 'tau', 'group': 'Privileged'}])])
+
+            tau = weighted_tau_nodes(base_scores, ranking, subgraph_nodes=minority_nodes, complementary_nodes=majority_nodes)
+            iter_results = pd.concat([iter_results, pd.DataFrame([{'value': tau, 'metric': 'tau', 'group': 'Unprivileged'}])])
+
+            exp = exposure_nodes(ranking, subgraph_nodes=majority_nodes)
+            iter_results = pd.concat([iter_results, pd.DataFrame([{'value': exp, 'metric': 'exposure', 'group': 'Privileged'}])])
+
+            exp = exposure_nodes(ranking, subgraph_nodes=minority_nodes)
+            iter_results = pd.concat([iter_results, pd.DataFrame([{'value': exp, 'metric': 'exposure', 'group': 'Unprivileged'}])])
+
+            tau = weighted_tau_separate_nodes(base_scores, ranking, subgraph_nodes=majority_nodes, complementary_nodes=minority_nodes)
+            iter_results = pd.concat([iter_results, pd.DataFrame([{'value': tau[0], 'metric': 'tau', 'group': 'Privileged within-group'}])])
+            iter_results = pd.concat([iter_results, pd.DataFrame([{'value': tau[1], 'metric': 'tau', 'group': 'Between groups'}])])
+
+            tau = weighted_tau_separate_nodes(base_scores, ranking, subgraph_nodes=minority_nodes, complementary_nodes=majority_nodes, calc_between=False)
+            iter_results = pd.concat([iter_results, pd.DataFrame([{'value': tau[0], 'metric': 'tau', 'group': 'Unprivileged within-group'}])])
+        
+            print(f'trial {trial}, {sampling_method}, {group_attribute}, {ranking_method}, {iteration} iterations.')
+
+        iter_results['trial'] = trial
+        iter_results['iteration'] = iteration
+        iter_results['sampling strategy'] = sampling_method
+        iter_results['recovery method'] = ranking_method
+        iter_results['group attribute'] = group_attribute
+        iter_results['benchmark'] = benchmark
+        results = pd.concat([results, iter_results], ignore_index=True)
+
+    return results
+
+
+
+
+if __name__ == '__main__':
+
+    # trial, benchmark
+    tasks = list(product([0], ['full dataset'],
+                         #['rankCentrality', 'randomRankRecovery', 'davidScore', 'fairPageRank'],
+                         ['GNNRank'],
+                         ['often_compared', 'often_first', 'often_formatted', 'open_source'],
+                         ['helm', 'alpaca', 'arena_hard']))
+
+    pool = multiprocessing.Pool()
+    results = pool.starmap(ranking_evaluation, tasks)
+
+    results = pd.concat(results, ignore_index=True)
+    results.to_csv('../fairpair/data/chatbot_arena_results/chatbotArena_GNNRank_fullDataset_evaluated.csv', index=False)
---
+++ chatbot_experiments.py
@@ -0,0 +1,148 @@
+import networkx as nx
+import numpy as np
+import pandas as pd
+import torch
+from tqdm.auto import tqdm
+
+from itertools import product
+import multiprocessing
+
+from fairpair import *
+
+import sys
+sys.path.append('../GNNRank/')
+from src.param_parser import ArgsNamespace # just import the class, not the parser
+from src.Trainer import Trainer
+
+
+def load_dataset(ground_truth_file:str, comparisons_file:str, group_attribute:str) -> nx.DiGraph:
+    
+    wins_df = pd.read_csv(comparisons_file, index_col=0)
+    benchmark_df = pd.read_csv(ground_truth_file, index_col=0) # model names as index
+
+    G = nx.from_pandas_adjacency(wins_df, create_using=nx.DiGraph)
+    G2 = G.subgraph(nodes=benchmark_df.index).copy()  # a smaller graph with only the models that we have benchmark data for
+
+    attr_df = benchmark_df[['score', group_attribute]].rename(columns={'score': 'skill'})
+    attr_df['unpriv'] = attr_df[group_attribute] == 0
+    attr_df = attr_df[['skill', 'unpriv']].to_dict(orient='index')
+
+    nx.set_node_attributes(G2, attr_df)
+
+    return G2
+
+
+def rank_full_dataset(
+        rank_using=rankCentrality,
+        group_attribute='long_response',
+        benchmark='helm',
+        comparisons_file='../fairpair/data/chatbot_arena/comparisons_cleaned.csv') -> list:
+    
+    ground_truth_file=f'../fairpair/data/chatbot_arena/{benchmark}_combined.csv'
+    
+    G = load_dataset(ground_truth_file, comparisons_file, group_attribute)
+    G = nx.convert_node_labels_to_integers(G)  # nodes need to be named 0…n in order for fairPageRank to work
+    G = FairPairGraph(G)
+    nx.set_edge_attributes(G, 1, 'weight')  # an edge either exists or it doesn't
+
+    ranker = RankRecovery(G)
+    ranks = pd.DataFrame(columns=['trial', 'iteration', 'skill score', 'rank', 'group',
+                                  'sampling method', 'ranker', 'group attribute', 'benchmark'])
+
+    if rank_using == 'fairPageRank':
+        current_proc = multiprocessing.current_process()
+        path = '../fairpair/data/fairPageRank/tmp_' + str(current_proc._identity[0])
+        ranking, other_nodes = ranker.apply(rank_using=rank_using, path=path)
+        ranker_name = 'fairPageRank'
+    else:
+        ranking, other_nodes = ranker.apply(rank_using=rank_using)
+        ranker_name = rank_using.__name__
+    
+    ranking_as_ranks = scores_to_rank(ranking, invert=False) # invert=True
+    for node, data in G.priv.nodes(data=True):
+        ranks = pd.concat([ranks, pd.DataFrame([[0, 0, data['skill'], ranking_as_ranks[node],
+                                                'Privileged', 'full dataset', ranker_name, group_attribute, benchmark]], columns=ranks.columns)])
+    for node, data in G.unpriv.nodes(data=True):
+        ranks = pd.concat([ranks, pd.DataFrame([[0, 0, data['skill'], ranking_as_ranks[node],
+                                                'Unprivileged', 'full dataset', ranker_name, group_attribute, benchmark]], columns=ranks.columns)])
+    
+    return ranks
+
+
+def rank_full_dataset_GNNRank(
+        group_attribute='long_response',
+        benchmark='helm',
+        comparisons_file='../fairpair/data/chatbot_arena/comparisons_cleaned.csv') -> list:
+    
+    ground_truth_file=f'../fairpair/data/chatbot_arena/{benchmark}_combined.csv'
+    
+    # customize `dataset` to properly save the model
+    # get optimal settings from the paper: `baseline`, `pretrain_with`, `train_with`, `upset_margin_coeff`
+    # use defaults for: `early_stopping`, `epochs`
+    # handle output cleverly using: `load_only=True`, `regenerate_data=True`, `be_silent=True`
+    # set K=10 for smaller dataset (used to be 20)
+    args = ArgsNamespace(AllTrain=True, ERO_style='uniform', F=70, Fiedler_layer_num=5, K=10, N=350, SavePred=False, all_methods=['DIGRAC', 'ib'],
+                     alpha=1.0, baseline='syncRank', cuda=True, data_path='/home/georg/fairpair/GNNRank/src/../data/',
+                     dataset=f'IMDB-WIKI_correlations/full_dataset', be_silent=True,
+                     debug=False, device=torch.device(type='cuda'), dropout=0.5, early_stopping=200, epochs=1000, eta=0.1, fill_val=0.5, hidden=8, hop=2,
+                     load_only=True, log_root='/home/georg/fairpair/GNNRank/src/../logs/', lr=0.01, no_cuda=False, num_trials=1, optimizer='Adam', p=0.05,
+                     pretrain_epochs=50, pretrain_with='dist', regenerate_data=True, season=1990, seed=31, seeds=[10], sigma=1.0, tau=0.5, test_ratio=1,
+                     train_ratio=1, train_with='proximal_baseline', trainable_alpha=False, upset_margin=0.01, upset_margin_coeff=0, upset_ratio_coeff=1.0, weight_decay=0.0005)
+    
+    G = load_dataset(ground_truth_file, comparisons_file, group_attribute)
+    G = nx.convert_node_labels_to_integers(G)  # nodes need to be named 0…n in order for fairPageRank to work
+    G = FairPairGraph(G)
+    nx.set_edge_attributes(G, 1, 'weight')  # an edge either exists or it doesn't
+
+    ranks = pd.DataFrame(columns=['trial', 'iteration', 'skill score', 'rank', 'group', 'sampling method', 'ranker', 'group attribute', 'benchmark'])
+
+    adj = nx.linalg.graphmatrix.adjacency_matrix(G, weight='weight') # returns a sparse matrix
+    trainer = Trainer(args, random_seed=10, save_name_base='test', adj=adj) # initialize with the given adjacency matrix
+    save_path_best, save_path_latest = trainer.train(model_name='ib')
+
+    score, pred_label = trainer.predict_nn(model_name='ib', model_path=save_path_best, A=None, GNN_variant='proximal_baseline')
+    ranking = {key: 1-score[0] for key, score in enumerate(score.cpu().detach().numpy())}
+    
+    ranking_as_ranks = scores_to_rank(ranking, invert=False) # invert=True
+    for node, data in G.priv.nodes(data=True):
+        ranks = pd.concat([ranks, pd.DataFrame([[0, 0, data['skill'], ranking_as_ranks[node],
+                                                'Privileged', 'full dataset', 'GNNRank', group_attribute, benchmark]], columns=ranks.columns)])
+    for node, data in G.unpriv.nodes(data=True):
+        ranks = pd.concat([ranks, pd.DataFrame([[0, 0, data['skill'], ranking_as_ranks[node],
+                                                'Unprivileged', 'full dataset', 'GNNRank', group_attribute, benchmark]], columns=ranks.columns)])
+    
+    return ranks
+
+
+# custom function to simulate starmap
+# see https://stackoverflow.com/a/67845088
+def full_dataset_starmap(args):
+    return rank_full_dataset(*args)
+
+def full_dataset_GNNRank_starmap(args):
+    return rank_full_dataset_GNNRank(*args)
+
+
+
+if __name__ == '__main__':
+
+    #tasks = list(product([randomRankRecovery, davidScore, rankCentrality, 'fairPageRank'],
+    #                     ['often_compared', 'often_first', 'often_formatted', 'open_source'],
+    #                     ['helm', 'alpaca', 'arena_hard'])) # rank_using, group_attribute, benchmark
+    
+    # for GNNRank
+    tasks = list(product(['often_compared', 'often_first', 'often_formatted', 'open_source'],
+                         ['helm', 'alpaca', 'arena_hard'])) # group_attribute, benchmark
+
+    try: multiprocessing.set_start_method('spawn') # if it wasn't alrady set, make sure we use the `spawn` method.
+    except RuntimeError: pass
+
+    with multiprocessing.Pool(processes=1) as pool:
+        pbar = tqdm(pool.imap(full_dataset_GNNRank_starmap, tasks), total=len(tasks))
+        results = list(pbar) # list invokes the evaluation
+    
+    ranks = pd.concat(results, ignore_index=True)
+
+    #ranks = rank_full_dataset_GNNRank('often_first', 'helm')
+
+    ranks.to_csv('../fairpair/data/chatbot_arena_results/GNNRank_correlations_full_dataset.csv', index=False)
